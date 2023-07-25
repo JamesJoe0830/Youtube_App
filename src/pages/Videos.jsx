46dd42f2f18e7d574f39994dfda0b5a2ab2fec12
james.joe <kyeun950830@gmail.com>
2023-07-26 00:23:58 +0900

    🔜Axios를 통한 API 받아오기

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import {useParams} from 'react-router-dom';
-import {useQuery} from '@tanstack/react-query';
-import VideoCard from '../components/VideoCard';
-export default function Videos() {
-    const {keyword} = useParams();
-    //🔜
-    const {isLoading, error, data: videos} = useQuery(
-        ['videos', keyword], async () => {return fetch(`/videos/${keyword ? 'search' : 'popluar'}.json`)
-        .then(res => res.json())
-        .then(data => data.items);
-    });
-     //🔜
-    return (
-        <div>
-            Videos {keyword ? `🔎${keyword}` : '🔥'}
-            {isLoading && <p>Loading ...</p>}
-            {error && <p>Somthing is wrong 😅</p>}
-            {videos && <ul>
-                {videos.map(video=> <VideoCard key ={video.id} video={video}/>)}    
-            </ul>}
-        </div>
-    );
-}
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import VideoCard from "../components/VideoCard";
+import axios from 'axios';
 
+export default function Videos() {
+  const { keyword } = useParams();
+  //🔜
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(["videos", keyword], async () => {
+    return axios
+    .get(`/videos/${keyword ? "search" : "popular"}.json`)
+    .then((res)=> res.data.items);
+    // fetch(`/videos/${keyword ? "search" : "popluar"}.json`)
+    //   .then((res) => res.json())
+    //   .then((data) => data.items);
+  });
+  //🔜
+  return (
+    <div>
+      Videos {keyword ? `🔎${keyword}` : "🔥"}
+      {isLoading && <p>Loading ...</p>}
+      {error && <p>Somthing is wrong 😅</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
